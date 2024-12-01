4bde3c4b93423c9a84a54eb08eeefa30d16a517f
Quach Dai Loi <lqu50@int.hrs.com>
2024-12-01 23:21:22 +0700

    [loi.quach] Add google tag manager

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,13 @@ import ReactDOM, { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { StrictMode } from "react";
+import TagManager, { TagManagerArgs } from "react-gtm-module";
+
+const tagManagerArgs: TagManagerArgs = {
+    gtmId: 'GTM-MJWVM5WH',
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
